ddad4a075d252c7a30988b15a51ae9a747a6ab6a
loris penelon <lpenelon@inception.lpenelon.42.fr>
2024-03-09 10:33:36 -0500

    update comments nginx

--- srcs/requirements/nginx/tools/nginx_script.sh
+++ srcs/requirements/nginx/tools/nginx_script.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+#generates self-assigned SSL/TLS certificate and private key using OpenSSL
+#req -x509 : generate self-signed SSL/TLS certificate signing request (CSR)
+#-nodes : dont encrypt private key with passphrase
+#-days : validity of the certificate
+#-newkey : new private key with length 4096 has to be generated
+#-keyout : where key will be saved
+#-out : where certificate will be saved
+#-subj : subject of ceritifcate (country, location, organization, organizational unit, common name)
+
+# check if already exist
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
-echo "Nginx: setting up ssl ...";
 openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=PT/ST=Lisbon/L=Lisbon/O=42 Lisboa/CN=lpenelon";
-echo "Nginx is set up with ssl/tls";
 fi
 
+# enable the CMD to run as the same process as the script. from PID1
 exec "$@"
